cf3dccbbecd6c934363d85eaa6b4ce13eda01eec
hapo-nghialt <nghia.lt@haposoft.com>
2021-10-18 23:34:09 +0700

    refactor

--- server/src/routes/v1/board.route.js
+++ server/src/routes/v1/board.route.js
@@ -4,10 +4,20 @@ import { BoardValidation } from '@/validations/board.validation'
 
 const router = express.Router()
 
-router.route('/')
-  .post(BoardValidation.createNew, BoardController.createNew)
+// router.route('/')
+//   .post(BoardValidation.createNew, BoardController.createNew)
 
-router.route('/:id')
-  .get(BoardController.getFullBoard)
+// router.route('/:id')
+//   .get(BoardController.getFullBoard)
+
+router.get('/:id', async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
+})
 
 export const boardRoutes = router
