089cab9e6fd7906b41ca1b30cb9b54c54bb9b933
hapo-nghialt <nghia.lt@haposoft.com>
2021-11-15 15:49:46 +0700

    commit

--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -3,6 +3,7 @@ import 'antd/dist/antd.css'
 import { Button, Card, Form, Input } from 'antd'
 import React, { useState } from 'react'
 import { Toast } from 'react-bootstrap'
+import './Register.scss'
 
 function Register() {
   const onFinish = async (values) => {
@@ -20,71 +21,74 @@ function Register() {
 
   return (
     <>
-      <Card
-        title="Default size card"
-        extra={<a href="#">More</a>}
-        style={{ maxWidth: 500 }}
-      >
-        <Form
-          name="basic"
-          labelCol={{
-            span: 4
+      <div className='register-page'>
+        <Card
+          className='register-card'
+          title='Đăng nhập vào Trello'
+          style={{
+            textAlign: 'center'
           }}
-          wrapperCol={{
-            span: 16
-          }}
-          className="pt-4"
-          onFinish={onFinish}
         >
-          <Form.Item
-            name="username"
-            rules={[
-              // {
-              //   required: true,
-              //   message: 'Please input your username!'
-              // },
-              // {
-              //   min: 3,
-              //   max: 20,
-              //   message: 'Username between 3 and 20 characters!'
-              // }
-            ]}
+          <Form
+            name='basic'
+            labelCol={{
+              span: 4
+            }}
+            wrapperCol={{
+              span: 16
+            }}
+            onFinish={onFinish}
           >
-            <Input placeholder='Enter your username'/>
-          </Form.Item>
-          <Form.Item
-            name="password"
-            // rules={[
-            //   {
-            //     required: true,
-            //     message: 'Please input your password!'
-            //   }
-            // ]}
-          >
-            <Input.Password
-              placeholder="Enter your password"
-            />
-          </Form.Item>
-          <Button type='primary' htmlType='submit'>
-            Submit
-          </Button>
-        </Form>
-        <Button onClick={handleShowToast}>
-          Click
-        </Button>
-        <Toast show={showToast}>
-          <Toast.Header>
-            <img
-              src="holder.js/20x20?text=%20"
-              className="rounded me-2"
-              alt=""
-            />
-            <strong className="me-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
-          </Toast.Header>
-          <Toast.Body>Woohoo, you are reading this text in a Toast!</Toast.Body>
-        </Toast>
-      </Card>
+            <Form.Item
+              name='username'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!'
+                },
+                {
+                  min: 3,
+                  max: 20,
+                  message: 'Username between 3 and 20 characters!'
+                }
+              ]}
+              style={{
+                width: '100%'
+              }}
+            >
+              <Input placeholder='Enter your username'/>
+            </Form.Item>
+            <Form.Item
+              name='password'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!'
+                }
+              ]}
+            >
+              <Input.Password
+                placeholder='Enter your password'
+              />
+            </Form.Item>
+            <Button type='primary' htmlType='submit' onClick={handleShowToast}>
+              Submit
+            </Button>
+          </Form>
+        </Card>
+      </div>
+      <Toast show={showToast} className='notifications'>
+        <Toast.Header>
+          <img
+            src='holder.js/20x20?text=%20'
+            className='rounded me-2'
+            alt=''
+          />
+          <strong className='me-auto'>Bootstrap</strong>
+          <small>11 mins ago</small>
+        </Toast.Header>
+        <Toast.Body>Woohoo, you are reading this text in a Toast!</Toast.Body>
+      </Toast>
     </>
   )
 }
