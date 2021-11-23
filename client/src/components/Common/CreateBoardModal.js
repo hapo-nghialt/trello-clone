760129176427f17e0eb9371eb37fc9fa46b15cde
hapo-nghialt <nghia.lt@haposoft.com>
2021-11-23 17:28:29 +0700

    commit

--- client/src/components/Common/CreateBoardModal.js
+++ client/src/components/Common/CreateBoardModal.js
@@ -4,13 +4,16 @@ import './Common.scss'
 
 function CreateBoardModal(props) {
   const { visible, handleOk, handleCancel } = props
+
+  const colors = ['red', 'blue', 'green', 'orange', 'yellow', 'black', 'gray', 'brown', 'pink']
+
   return (
     <Modal className='create-board-modal' visible={visible} onOk={handleOk} onCancel={handleCancel}>
       <div>
         <input placeholder='Add board title' />
       </div>
-      <ul>
-
+      <ul className='list-button'>
+        {colors.map((color, id) => <li key={id} style={{ backgroundColor: color }}></li>)}
       </ul>
     </Modal>
   )
